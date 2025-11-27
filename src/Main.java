a6db730eb4a9a597f051556e9bb1195e552f3e96
EugeneDokyeAnokye <yawanokye99@gmail.com>
2025-11-27 11:33:26 +0000

    Added Sample Accounts

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        createSampleAccounts();
+        createSampleAccounts();
 
         int choice;
         do {
